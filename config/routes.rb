12dc098452561e21e0aaee82a99ffd715781a517
summerlove91 <gkdus3040@gmail.com>
2017-07-30 18:10:46 -0700

    Rails: Authentication - User Model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     #  resources :sponsored_posts, except: [:index]
    end
 
+   resources :users, only: [:new, :create]
+
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728082911) do
+ActiveRecord::Schema.define(version: 20170730035346) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 20170728082911) do
     t.string "name"
     t.boolean "public", default: true
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
