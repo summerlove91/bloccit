a466adf4814194c3910a031bebb1c5211c0db805
summerlove91 <gkdus3040@gmail.com>
2017-08-02 11:08:28 -0700

    Rails:Authentication Sign-in ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
      @user = User.new
    end
 
+   def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+   end
+
    def create
      @user = User.new
      @user.name = params[:user][:name]
@@ -13,6 +21,7 @@ class UsersController < ApplicationController
 # @post = Post.new({:title => params[:title], :body => params[:body]})
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+       create_session(@user)
        redirect_to root_path
      else
        flash.now[:alert] = "There was an error creating your account. Please try again."
